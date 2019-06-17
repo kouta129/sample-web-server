28f5ff9d1712249eb6af9db367a86dc3b5229a66
kouta isikawa <kouta09217@gmail.com>
2019-06-17 12:29:41 +0900

    6/14 課題

--- mywebapi/index.js
+++ mywebapi/index.js
@@ -4,8 +4,19 @@ const express = require('express');
 // expressアプリを作成する
 const app = express();
 
-// ルート(http://localhost/)にアクセスしたときに[Hello]を返す
-app.get('/',(req,res) => res.send('Hello'));
+// http://localhost:3000/api/v1/list にアクセスした時に
+// TODOリストを返す
+app.get('/api/v1/list', (req,res) => {
+    // クライアントに送るJSONデータ
+　　const todoList = [
+        { title: 'JavaScripを勉強する', done: true } ,
+        { title: 'Node.jsを勉強する', done: false },
+        { title: 'Web APIを作る', done: false }
+    ];
+
+    // JSONを送信する
+　  res.json(todoList);
+});
 
 // ポート3000でサーバ立てる
 app.listen(3000, () => console.log('Listening on port 3000'));
